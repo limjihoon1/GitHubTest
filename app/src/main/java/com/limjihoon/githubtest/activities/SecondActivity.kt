e95408d9fc582a1b3e8ec7494145851cd55309f6
dlawlgns1994 <dlawlgns1994@gmail.com>
2024-03-11 13:40:54 +0900

    add SecondActivity and image

---
+++ app/src/main/java/com/limjihoon/githubtest/activities/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.limjihoon.githubtest.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.limjihoon.githubtest.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
